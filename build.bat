0350fdc542d35c1d97cef19fcd55a7dd9844ab4b
Jason Light <rilwal2@gmail.com>
2015-07-04 08:20:19 -0700

    updated build script for Jenkins

--- build.bat
+++ build.bat
@@ -5,13 +5,9 @@ setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat"
 set PATH=%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin;C:\Program Files\7-Zip;
 
-set /p buildnumber=<BuildNumber.txt
-set /a next="buildnumber+1"
-echo %next% > BuildNumber.txt
+set buildnumber=%1
 
 echo Starting build %buildnumber%
-
-git pull
 
 if exist build rmdir /s /q build
 mkdir build
@@ -27,39 +23,5 @@ mkdir build\x64
 msbuild .\platform-game\platform-game\platform-game.vcxproj /m /p:Configuration=Release /p:Platform=x64 > .\build\x64\build.log
 copy platform-game\x64\Release\platform-game.exe build\x64
 copy lib\x64\* build\x64 > NUL
-
-echo Building Assets
-if not exist ..\platform-game-art-assets git clone https://github.com/rilwal/platform-game-art-assets ..\platform-game-art-assets
-pushd ..\platform-game-art-assets
-git pull
-call build.bat
-popd
-
-mkdir build\x64\assets
-copy ..\platform-game-art-assets\build\* build\x64\assets > NUL
-
-mkdir build\x86\assets
-copy ..\platform-game-art-assets\build\* build\x86\assets > NUL
-
-echo Packaging build
-set file_32=platform-game-x86-%buildnumber%.zip
-set file_64=platform-game-x64-%buildnumber%.zip
-
-set upload_dest=root@rilwal.com:/srv/www/fs.abscission.net/builds
-
-pushd build
-cd x86
-7z a %file_32% *
-copy %file_32% Z:\
-scp %file_32% %upload_dest%
-cd ..
-cd x64
-7z a %file_64% *
-copy %file_64% Z:\
-scp %file_64% %upload_dest%
-cd ..
-popd
-
-sh notify.sh "A new build is available! Get it at:\n32-bit: http://fs.abscission.net/builds/%file_32%\n64-bit: http://fs.abscission.net/builds/%file_64%"
 
 endlocal
